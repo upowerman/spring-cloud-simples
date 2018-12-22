86243dc064bd5ba5abc529ed5e2f82f4a1bafd74
gaoyunfeng <gaoyunfeng@lanxum.com>
2018-12-22 16:50:28 +0800

    apollo 客户端配置

---
+++ gateway/gateway-zuul-apollo/src/main/java/com/yunus/ZuulServerApplication.java
@@ -0,0 +1,22 @@
+package com.yunus;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * @author lanxum
+ */
+@EnableApolloConfig
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
