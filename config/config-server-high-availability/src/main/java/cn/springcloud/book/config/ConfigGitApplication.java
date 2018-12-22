cae64a54e18ed52698b0dc335abcb19a67dbe7b6
gaoyunfeng <gaoyunfeng@lanxum.com>
2018-12-22 16:11:26 +0800

    配置中心，服务端高可用

---
+++ config/config-server-high-availability/src/main/java/cn/springcloud/book/config/ConfigGitApplication.java
@@ -0,0 +1,20 @@
+package cn.springcloud.book.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author lanxum
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigGitApplication.class, args);
+
+    }
+}
