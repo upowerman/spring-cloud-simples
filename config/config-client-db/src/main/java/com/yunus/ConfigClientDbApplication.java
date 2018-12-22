f76be8f59d7aefcae2f1e82aac4f2f22e2254c20
gaoyunfeng <gaoyunfeng@lanxum.com>
2018-12-22 13:22:58 +0800

    spring config 从数据库读取配置

---
+++ config/config-client-db/src/main/java/com/yunus/ConfigClientDbApplication.java
@@ -0,0 +1,17 @@
+package com.yunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lanxum
+ */
+@SpringBootApplication
+public class ConfigClientDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientDbApplication.class, args);
+    }
+
+}
+
